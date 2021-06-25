0009d7d5af910e1cc3ee1d974b7bd31bcefb469a
Anuj Gautam <65212053+AnujGautam321@users.noreply.github.com>
2021-06-25 09:29:57 +0545

    Add files via upload

---
+++ code.js
@@ -0,0 +1,16 @@
+function fucn(val)
+{
+    document.getElementById("display").value+=val;
+    
+}
+function solve()
+{
+    let a = document.getElementById("display").value;
+    let b = eval(a);
+    document.getElementById("display").value = b;
+
+}
+function clr()
+{
+    document.getElementById("display").value = "";
+}
